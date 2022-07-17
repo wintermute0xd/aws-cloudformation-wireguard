d0d58bec337fabec9c86434cb51342511bd59c7a
wintermute0xd <v110113@gmail.com>
2022-07-17 22:21:32 +0200

    add user script improved

--- adduser.ps1
+++ adduser.ps1
@@ -40,9 +40,24 @@ $ClientPubKey = Read-Host
 Write-Host -ForegroundColor Gray -BackgroundColor Black "# IP address from wireguard client config
 # Allowed range from 10.10.10.11 to 10.10.10.254
 # Note that no two peers can have the same Allowed IP setting"
+Write-Host -ForegroundColor DarkMagenta -BackgroundColor Black "Already taken IPs:"
+# Run 'sudo wg' on remote server and grep ip addresses from output
+# -o show only matched -P use Perl regexp
+# ?<= search but exclude from output
+$Command = "sudo wg | grep -oP '(?<=allowed ips: )\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2}'"
+# Out-String convert output to string
+$IPTaken = ssh -i $SSHKey ubuntu@${ServerIP} $Command | Out-String
+
+if ($IPTaken -eq ''){
+    Write-Host -ForegroundColor Green -BackgroundColor Black "No IP taken"
+}
+else {
+    Write-Host -BackgroundColor Black -ForegroundColor DarkMagenta ($IPTaken -replace " +", "`n")
+}
 Write-Host -ForegroundColor Blue -BackgroundColor Black "- Allowed Client IP: " -NoNewline
 $AllowedIPs = Read-Host
 
+# Run command on remote server to add peer
 $Command = "sudo wg set wg0 peer $ClientPubKey allowed-ips $AllowedIPs"
 ssh -i $SSHKey ubuntu@${ServerIP} $Command
 
